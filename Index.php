46881bf7c220fae0adcd944dca137766abca82e2
Mya Anastasya Silaban <90168459+myanastasya@users.noreply.github.com>
2021-10-17 21:16:32 +0700

    Delete Index.php

--- Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
-echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
-
-?>
